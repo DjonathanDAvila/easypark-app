6566d307d81a0b341d8e9e5b9318ef5470291024
Cairo Augusto De Andrade <107778041+CairoDeAndrade@users.noreply.github.com>
2024-11-02 15:00:37 -0300

    feat: Tax configuration path redirection

---
+++ src/app/pages/tax_configuration/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import AppBar from '../../components/components/ResponsiveAppBar/ResponsiveAppBar';
+
+const HomePage = () => {
+  return (
+    <div>
+        <AppBar></AppBar>
+        <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default HomePage;
